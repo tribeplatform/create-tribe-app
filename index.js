3278cd54010581e51973d56b6f406c3286b2aa24
Amir Ahangari <amir@ahangari.org>
2022-04-14 20:47:47 +0400

    Refactor index.js

--- index.js
+++ index.js
@@ -2,34 +2,50 @@
 const prompt = require("prompt-sync")({ sigint: true });
 const shell = require("shelljs");
 const fs = require("fs");
+const { exit } = require("process");
 const githubURL = "http://github.com/tribeplatform/tribe-starter-app";
-function main() {
-  const name = prompt("Name:");
-  const directory = prompt("Directory:");
-
+function cloneGit(name) {
   if (!shell.which("git")) {
     shell.echo("Sorry, this script requires git");
     shell.exit(1);
   }
-
-  shell.cd(directory);
   if (shell.exec(`git clone ${githubURL} ${name}`).code !== 0) {
     shell.echo("Error: Git clone failed. Please retry.");
     shell.exit(1);
   }
+}
 
-  shell.cd(name);
-
-  // Remove git history
+function initGit() {
   shell.rm("-rf", ".git");
-
-  const pkgJson = require("./package.json");
-  pkgJson.name = name;
-  fs.writeFileSync("package.json", JSON.stringify(pkgJson, null, 2));
-
   shell.exec("git init");
   shell.exec("git add -A");
   shell.exec(`git commit -m "Initial commit"`);
+}
+
+function modifyProject(name) {
+  directory = shell.pwd().stdout;
+  shell.cp("server/.env.example", "server/.env");
+  shell.cp("client/.env.example", "client/.env");
+  const pkgJson = require(`${directory}/package.json`);
+  pkgJson.name = name;
+  fs.writeFileSync(
+    `${directory}/package.json`,
+    JSON.stringify(pkgJson, null, 2)
+  );
+}
+
+function main() {
+  const name = prompt("Name:");
+  let directory = prompt("Directory:");
+  shell.cd(directory);
+
+  cloneGit(name);
+
+  shell.cd(name);
+
+  modifyProject(name);
+
+  initGit();
 }
 
 if (require.main === module) {
